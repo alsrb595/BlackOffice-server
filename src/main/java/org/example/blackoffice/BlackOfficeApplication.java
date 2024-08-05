9059f64dcc679bbbb9f4c96af480214c732dc49a
alsrb595 <alsrb595@gmail.com>
2024-08-05 17:30:12 +0900

    first commit

---
+++ src/main/java/org/example/blackoffice/BlackOfficeApplication.java
@@ -0,0 +1,13 @@
+package org.example.blackoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/blackoffice/BlackOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.blackoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
